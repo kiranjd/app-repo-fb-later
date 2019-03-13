10a51d78a75572cc62ea326b71cdf5336d6f9b33
Kiran JD <kiranjd8@gmail.com>
2019-03-14 04:59:53 +0530

    camera timer, record button color

--- app/components/goLive/goLive.js
+++ app/components/goLive/goLive.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Alert, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Alert, StatusBar, ToastAndroid } from 'react-native';
 import { Button, Overlay } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { NodeCameraView } from 'react-native-nodemediaclient';
 import Orientation from 'react-native-orientation';
+import { Stopwatch, Timer } from 'react-native-stopwatch-timer'
 //import { AndroidBackHandler } from 'react-navigation-backhandler';
 
 export default class GoLive extends Component {
@@ -15,11 +16,25 @@ export default class GoLive extends Component {
             isPublish: false,
             time: '00:00',
             color: 'red',
+            stopwatchStart: false,
+            stopwatchReset: false,
         }
     }
 
     componentDidMount() {
         Orientation.lockToLandscape();
+    }
+
+    getFormattedTime(time) {
+        this.currentTime = time;
+    };
+
+    toggleStopwatch() {
+        this.setState({ stopwatchStart: !this.state.stopwatchStart, stopwatchReset: false });
+    };
+
+    resetStopwatch() {
+        this.setState({ stopwatchStart: false, stopwatchReset: true });
     }
 
     render() {
@@ -37,7 +52,7 @@ export default class GoLive extends Component {
         }
         return (
             <View style={{ flex: 1 }}>
-            <StatusBar hidden={true}/>
+                <StatusBar hidden={true} />
                 <Overlay
                     isVisible={true}
                     overlayStyle={{
@@ -49,58 +64,65 @@ export default class GoLive extends Component {
                     }}
                     fullScreen={true}
                 >
-                <View>
-                    <Text style={{color: 'white', top: '0%', textAlign: 'left', fontSize: 30}}>{this.state.time}</Text>
-                </View>
-                <View style={{flexDirection: 'row', marginTop: '35%', left: '80%'}}>
-                <TouchableOpacity>
-                    <Button
-                        buttonStyle={recordButtonStyle()}
-                        onPress={() => {
-                            if (this.state.isPublish) {
-                                this.setState({
-                                    publishBtnTitle: 'Start Publish',
-                                    isPublish: false,
-                                    color: 'red'
-                                });
-                                this.vb.stop();
-                                ToastAndroid.show('Publish Ended', ToastAndroid.LONG);
-                                this.props.navigation.navigate('Home')
-                            } else {
-                                this.setState({
-                                    publishBtnTitle: 'Stop Publish',
-                                    isPublish: true,
-                                    color: 'green'
-                                });
-                                this.vb.start();
-                                ToastAndroid.show('Publish Started', ToastAndroid.SHORT);
-                            }
-                        }}
-                        //title={this.state.publishBtnTitle}
-                        color="#841584"
-                    />
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                    <Button
-                        onPress={() => {
-                            this.vb.switchCamera();
-                        }}
-                        icon={
-                            <Icon
-                                name="refresh"
-                                size={45}
-                                color="white"
+                    {this.state.isPublish && <View>
+                        <Stopwatch start={this.state.stopwatchStart}
+                            reset={this.state.stopwatchReset}
+                            options={options}
+                            getTime={this.getFormattedTime} />
+                    </View>}
+
+                    {!this.state.isPublish && <View style={{height: 37}}></View>}
+                    <View style={{ flexDirection: 'row', top: '35%', left: '80%' }}>
+                        <TouchableOpacity>
+                            <Button
+                                buttonStyle={recordButtonStyle()}
+                                onPress={() => {
+                                    if (this.state.isPublish) {
+                                        this.setState({
+                                            publishBtnTitle: 'Start Publish',
+                                            isPublish: false,
+                                            color: 'red'
+                                        });
+                                        this.resetStopwatch();
+                                        this.vb.stop();
+                                        ToastAndroid.show('Publish Ended', ToastAndroid.LONG);
+                                        this.props.navigation.navigate('Home')
+                                    } else {
+                                        this.setState({
+                                            publishBtnTitle: 'Stop Publish',
+                                            isPublish: true,
+                                            color: 'green'
+                                        });
+                                        this.toggleStopwatch();
+                                        this.vb.start();
+                                        ToastAndroid.show('Publish Started', ToastAndroid.SHORT);
+                                    }
+                                }}
+                                //title={this.state.publishBtnTitle}
+                                color="#841584"
                             />
-                        }
-                        buttonStyle={{
-                            height: hp('8%'),
-                            width: hp('8%'),
-                            backgroundColor: 'transparent',
-                            marginTop: hp('0.5%'),
-                            marginLeft: wp('14%')
-                        }}
-                    />
-                    </TouchableOpacity>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Button
+                                onPress={() => {
+                                    this.vb.switchCamera();
+                                }}
+                                icon={
+                                    <Icon
+                                        name="refresh"
+                                        size={45}
+                                        color="white"
+                                    />
+                                }
+                                buttonStyle={{
+                                    height: hp('8%'),
+                                    width: hp('8%'),
+                                    backgroundColor: 'transparent',
+                                    marginTop: hp('0.5%'),
+                                    marginLeft: wp('14%')
+                                }}
+                            />
+                        </TouchableOpacity>
                     </View>
                 </Overlay>
 
@@ -117,3 +139,17 @@ export default class GoLive extends Component {
         );
     }
 }
+
+const options = {
+    container: {
+        backgroundColor: 'rgba(0,0,0,0.4)',
+        padding: 3,
+        borderRadius: 100,
+        width: 110,
+    },
+    text: {
+        fontSize: 20,
+        color: '#FFF',
+        marginLeft: 7,
+    }
+};
