a34acb8ff3d98c088fbcf60172542be6c0723e0f
Kiran JD <kiranjd8@gmail.com>
2019-04-13 03:13:27 +0530

    dates are marked

--- app/components/calendar/calendar.js
+++ app/components/calendar/calendar.js
@@ -1,105 +1,90 @@
 import React, { Component } from 'react';
-import {
-  Text,
-  View,
-  StyleSheet
-} from 'react-native';
-import {Agenda} from 'react-native-calendars';
+import { StyleSheet, Text, View, Image, TouchableOpacity, BackHandler, ActivityIndicator } from 'react-native';
+import { CalendarList } from 'react-native-calendars';
+import { heightPercentageToDP } from 'react-native-responsive-screen';
+import HeaderBar from '../common/headerBar';
+import firebase from 'react-native-firebase';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+//
 
-export default class AgendaScreen extends Component {
+export default class calendars extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: {}
-    };
+      dates: null, 
+      user: [],
+      isLoading: true
+    }
   }
+
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      this.props.navigation.navigate('Home');
+      return true;
+    });
+  }
+
+  componentWillMount() {
+    this.unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+          this.setState({
+              user: user.toJSON(),
+          });
+      } else {
+          this.setState({
+              user: null,
+          });
+      }
+  });
+  }
+
+  componentDidMount() {
+    let userLocal = this.state.user;
+    let dates = new Object();
+    let url = `http://139.59.69.143/api/markDates.php?uid=${userLocal.uid}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+          responseJson.forEach(element => {
+            dates[element] = { selected: true, selectedColor: 'orange' };
+          });
+          
+          this.setState({ dates: dates, isLoading: false });
+      })
+  }
+
+
 
   render() {
+    if (this.state.isLoading) {
+      return (
+          <View style={styles.container}>
+              <HeaderBar pageName='Alloted Classes' navigation={this.props.navigation} />
+              <ActivityIndicator size="large" color="#000" style={{ marginTop: hp('40%') }} />
+          </View>
+      );
+  }
     return (
-      <Agenda
-        items={this.state.items}
-        loadItemsForMonth={this.loadItems.bind(this)}
-        selected={'2017-05-16'}
-        renderItem={this.renderItem.bind(this)}
-        renderEmptyDate={this.renderEmptyDate.bind(this)}
-        rowHasChanged={this.rowHasChanged.bind(this)}
-        // markingType={'period'}
-        // markedDates={{
-        //    '2017-05-08': {textColor: '#666'},
-        //    '2017-05-09': {textColor: '#666'},
-        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
-        //    '2017-05-21': {startingDay: true, color: 'blue'},
-        //    '2017-05-22': {endingDay: true, color: 'gray'},
-        //    '2017-05-24': {startingDay: true, color: 'gray'},
-        //    '2017-05-25': {color: 'gray'},
-        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
-         // monthFormat={'yyyy'}
-         // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
-        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
-      />
-    );
-  }
-
-  loadItems(day) {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = this.timeToString(time);
-        if (!this.state.items[strTime]) {
-          this.state.items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 5);
-          for (let j = 0; j < numItems; j++) {
-            this.state.items[strTime].push({
-              name: 'Item for ' + strTime,
-              height: Math.max(50, Math.floor(Math.random() * 150))
-            });
-          }
-        }
-      }
-      //console.log(this.state.items);
-      const newItems = {};
-      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
-      this.setState({
-        items: newItems
-      });
-    }, 1000);
-    // console.log(`Load Items for ${day.year}-${day.month}`);
-  }
-
-  renderItem(item) {
-    return (
-      <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
-    );
-  }
-
-  renderEmptyDate() {
-    return (
-      <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
-    );
-  }
-
-  rowHasChanged(r1, r2) {
-    return r1.name !== r2.name;
-  }
-
-  timeToString(time) {
-    const date = new Date(time);
-    return date.toISOString().split('T')[0];
+      <View style={styles.container}>
+        <HeaderBar pageName = 'Alloted Classes' navigation = {this.props.navigation} />
+        <CalendarList style={styles.calendarContainer}
+          horizontal={true}
+          pastScrollRange={1}
+          futureScrollRange={1}
+          markedDates={this.state.dates}
+        />
+      </View>
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  item: {
-    backgroundColor: 'white',
-    flex: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginRight: 10,
-    marginTop: 17
+  container: {
+    height: hp('50%'),
   },
-  emptyDate: {
-    height: 15,
-    flex:1,
-    paddingTop: 30
+  calendarContainer: {
+    borderWidth: 0.7,
+    borderColor: 'black',
   }
-});
+})
